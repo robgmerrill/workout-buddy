52a0603e3dee8dfef597046ebcfcf962f7e4aace
Rob Merrill <robgmerrill@gmail.com>
2023-06-19 08:19:28 -0700

    add proxy for backend, fetch data from backend and pass to workout details component

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react"
+import WorkoutDetails from "../components/WorkoutDetails";
+
 export default function Home() {
+    const [workouts, setWorkouts] = useState(null);
+
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const res = await fetch('/api/workouts');
+            if (res.ok) {
+                const data = await res.json();
+                console.log(data);
+                setWorkouts(data);
+            }
+        }
+        fetchWorkouts();
+    }, [])
+
     return (
-        <div class="home">
-            <h2>Home</h2>
+        <div className="home">
+            <div className="workouts">
+                {workouts && workouts.map(workout => ( 
+                <WorkoutDetails key={workout.id} workout={workout} />
+                    )
+                )}
+            </div>
         </div>
     )
 }

c58212c3abf55627aa1bb25d92f35ee8a8b2e41c
Rob Merrill <robgmerrill@gmail.com>
2023-06-19 20:37:45 -0700

    add new workout form

---
+++ frontend/src/components/WorkoutForm.js
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+export default function WorkoutForm() {
+    const [title, setTitle] = useState('');
+    const [reps, setReps] = useState('');
+    const [load, setLoad] = useState('');
+    const [error, setError] = useState(null);
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        const workout = { title, load, reps };
+        console.log(workout);
+        const res = await fetch('/api/workouts', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(workout)
+        })
+        const json = await res.json();
+        console.log(json);
+        if (!res.ok) {
+            setError(json.error);
+        }
+        if (res.ok) {
+            setTitle('');
+            setReps('');
+            setLoad('');
+            setError(null);
+            console.log('new workout added', json);
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit} className="create">
+            <h3>Add a New Workout</h3>
+            <label>Exercise Title:</label>
+            <input 
+                type="text"
+                onChange={(e) => setTitle(e.target.value)}
+                value={title}
+            />
+            <label>Load (in Kg):</label>
+            <input 
+                type="number"
+                onChange={(e) => setLoad(e.target.value)}
+                value={load}
+            />
+            <label>Reps:</label>
+            <input 
+                type="number"
+                onChange={(e) => setReps(e.target.value)}
+                value={reps}
+            />
+            {error && <div className="error">{error}</div>}
+            <button>Add Workout</button>
+        </form>
+    )
+}
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import WorkoutDetails from "../components/WorkoutDetails";
+import WorkoutForm from "../components/WorkoutForm";
 
 export default function Home() {
     const [workouts, setWorkouts] = useState(null);
@@ -24,6 +25,7 @@ export default function Home() {
                     )
                 )}
             </div>
+            <WorkoutForm />
         </div>
     )
 }

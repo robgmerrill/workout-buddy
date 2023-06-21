6ed6faec1ccca8ee52bebf69e559360a9e2c45dc
Rob Merrill <robgmerrill@gmail.com>
2023-06-21 06:57:08 -0700

    backend tets

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -1,34 +1,58 @@
+// Import useState hook from React to handle state management for the form fields.
 import { useState } from "react";
+
+// Import custom hook that provides context for managing workout-related state.
 import useWorkoutsContext from "../hooks/useWorkoutsContext";
 
+// Define WorkoutForm as a functional component.
 export default function WorkoutForm() {
+
+    // Extract the dispatch function from the workouts context.
     const { dispatch } = useWorkoutsContext();
+
+    // Declare state variables to manage form field values.
     const [title, setTitle] = useState('');
     const [reps, setReps] = useState('');
     const [load, setLoad] = useState('');
     const [error, setError] = useState(null);
 
+    // Define function to handle form submission.
     async function handleSubmit(e) {
+
+        // Prevent the default form submission behavior.
         e.preventDefault();
+
+        // Create workout object from state values.
         const workout = { title, load, reps };
+
+        // Send POST request to create a new workout in the backend.
         const res = await fetch('/api/workouts', {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json' // Declare content type of the request body.
             },
-            body: JSON.stringify(workout)
+            body: JSON.stringify(workout) // Convert workout object to JSON string format.
         })
+
+        // Parse response as JSON.
         const json = await res.json();
+
+        // Log the response to the console.
         console.log(json);
+
+        // Check if the request was not successful, if so, update error state.
         if (!res.ok) {
             setError(json.error);
         }
+
+        // If the request was successful, reset all form fields and error state.
         if (res.ok) {
             setTitle('');
             setReps('');
             setLoad('');
             setError(null);
 
+            // Log the added workout and dispatch the CREATE_WORKOUT action to update context state.
             console.log('new workout added', json);
             dispatch({
                 type: 'CREATE_WORKOUT',
@@ -37,28 +61,39 @@ export default function WorkoutForm() {
         }
     }
 
+    // Render the form.
     return (
         <form onSubmit={handleSubmit} className="create">
             <h3>Add a New Workout</h3>
+
+            {/* Text input field for exercise title. */}
             <label>Exercise Title:</label>
             <input 
                 type="text"
-                onChange={(e) => setTitle(e.target.value)}
-                value={title}
+                onChange={(e) => setTitle(e.target.value)} // Update title state on change.
+                value={title} // Bind input field value to title state.
             />
+
+            {/* Number input field for load. */}
             <label>Load (in Kg):</label>
             <input 
                 type="number"
-                onChange={(e) => setLoad(e.target.value)}
-                value={load}
+                onChange={(e) => setLoad(e.target.value)} // Update load state on change.
+                value={load} // Bind input field value to load state.
             />
+
+            {/* Number input field for reps. */}
             <label>Reps:</label>
             <input 
                 type="number"
-                onChange={(e) => setReps(e.target.value)}
-                value={reps}
+                onChange={(e) => setReps(e.target.value)} // Update reps state on change.
+                value={reps} // Bind input field value to reps state.
             />
+
+            {/* If error state is not null, display error message. */}
             {error && <div className="error">{error}</div>}
+
+            {/* Submit button for form. */}
             <button>Add Workout</button>
         </form>
     )
--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -1,39 +1,42 @@
+// Import createContext to create a new context and useReducer to handle state logic.
 import { createContext, useReducer } from "react";
 
-// Create a context object
+// Create a context object. This object will hold the context for our application.
 const WorkoutContext = createContext();
 
-// Create a provider for components to consume and subscribe to changes
-    // state is initialls {workouts: null}
+// Define a reducer function for handling changes to the workout state.
 const workoutsReducer = (state, action) => {
+    // Depending on the action type, we update our state.
     switch (action.type) {
         case 'SET_WORKOUTS':
+            // If the action is 'SET_WORKOUTS', we set the state to the new list of workouts.
             return {
                 workouts: action.payload,
             }
         case 'CREATE_WORKOUT':
+            // If the action is 'CREATE_WORKOUT', we add the new workout to the existing list of workouts.
             return {
                 workouts: [action.payload, ...state.workouts],
             }
         default:
+            // If the action is not recognized, we return the existing state unchanged.
             return state;
     }
 }
 
-// Create a provider for components to consume and subscribe to changes
+// Create a provider function to wrap components that will have access to the context.
 export function WorkoutContextProvider({children}) {
-    // Create the initial state
+    // Use the useReducer hook to initialize our state and provide the dispatch function.
     const [state, dispatch] = useReducer(workoutsReducer, {workouts: null})
 
-
+    // Return the Provider component provided by createContext.
+    // This will wrap any child components and provide them with access to our context.
     return (
-        // value prop is where we define what values
         <WorkoutContext.Provider value={{...state, dispatch}}>
-            {/* everything that is wrapped */}
             {children}
         </WorkoutContext.Provider>
     )
 }
 
-// Export it so it can be used by other Components
+// Export the context object so it can be used by other components via the useContext hook.
 export default WorkoutContext;
